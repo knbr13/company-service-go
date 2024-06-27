60e614056b3a211a87dc9f9793a3d05e81d131d3
knbr13 <abdullahalaadine63@gmail.com>
2024-06-28 02:37:08 +0300

    update kafka client version

--- internal/kafka/kafka.go
+++ internal/kafka/kafka.go
@@ -9,6 +9,7 @@ func ConnectProducer(brokers []string) (sarama.SyncProducer, error) {
 	config.Producer.Return.Successes = true
 	config.Producer.RequiredAcks = sarama.WaitForAll
 	config.Producer.Retry.Max = 5
+	config.Version = sarama.V3_6_0_0
 
 	conn, err := sarama.NewSyncProducer(brokers, config)
 	if err != nil {
