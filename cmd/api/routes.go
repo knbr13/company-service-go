70d93fc193a6933ef376714fd5338c47b33b7ec0
knbr13 <abdullahalaadine63@gmail.com>
2024-06-27 08:10:00 +0300

    add companies routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -4,13 +4,22 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/knbr13/company-service-go/cmd/api/middlewares"
 )
 
 func (app *app) SetupRoutes() http.Handler {
 	r := chi.NewRouter()
+	mdlws := middlewares.NewMiddlewares(app.cfg)
 
 	r.Post("/register", app.hndlrs.Users.Register)
 	r.Post("/login", app.hndlrs.Users.Login)
 
+	r.Get("/companies/{id}", app.hndlrs.Companies.GetCompany)
+	r.Group(func(r chi.Router) {
+		r.Use(mdlws.JWTMiddleware)
+		r.Post("/companies", app.hndlrs.Companies.Create)
+		r.Patch("/companies/{id}", app.hndlrs.Companies.Update)
+		r.Delete("/companies/{id}", app.hndlrs.Companies.Delete)
+	})
 	return r
 }
